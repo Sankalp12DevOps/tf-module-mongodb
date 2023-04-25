2470bfb527aefb6a5134ed191c388ce4d15a9c1f
Sankalp12DevOps <sravanthi388@gmail.com>
2023-04-25 21:35:29 +0530

    m

--- data.tf
+++ data.tf
@@ -1,6 +1,6 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
-  config {
+  config = {
     bucket = "my-terraform-learn"
     key = "${var.ENV}/terraform/terraform.tfstate"
     region = "ap-south-1"
