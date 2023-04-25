c64c869827c6e46680a93fa25804247784624b7b
Sankalp12DevOps <sravanthi388@gmail.com>
2023-04-25 22:21:18 +0530

    m

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_docdb_cluster" "docdb" {
+resource "aws_docdb_cluster" "default" {
   cluster_identifier      = "my-docdb-cluster"
   engine                  = "docdb"
   master_username         = "admin1"
@@ -14,4 +14,11 @@ resource "aws_docdb_subnet_group" "default" {
   tags = {
     Name = "My docdb subnet group"
   }
+}
+
+resource "aws_docdb_cluster_instance" "cluster_instances" {
+  count              = 1
+  identifier         = "roboshop-${var.ENV}-docdb-instance"
+  cluster_identifier = aws_docdb_cluster.default.id
+  instance_class     = "db.t3.medium"
 }
