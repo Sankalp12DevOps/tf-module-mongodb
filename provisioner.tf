76eb6171018fe469951571703965c088b8930361
Sankalp12DevOps <sravanthi388@gmail.com>
2023-04-28 15:20:23 +0530

    m

--- provisioner.tf
+++ provisioner.tf
@@ -6,6 +6,7 @@ resource "null_resource" "web" {
     cd /tmp
     unzip mongodb.zip
     cd mongodb-main
+    wget https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
     mongo --ssl --host ${aws_docdb_cluster.default.endpoint} --sslCAFile rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < catalogue.js
     mongo --ssl --host ${aws_docdb_cluster.default.endpoint} --sslCAFile rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < users.js
     EOF
