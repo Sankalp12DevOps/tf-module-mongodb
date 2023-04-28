8487d3a52af0b6099daacb5fc00ee34c7f939076
Sankalp12DevOps <sravanthi388@gmail.com>
2023-04-28 15:11:40 +0530

    m

--- provisioner.tf
+++ provisioner.tf
@@ -1,12 +1,11 @@
 resource "null_resource" "web" {
   provisioner "local-exec" {
     command = <<EOF
-    wget https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
     curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
     cd /tmp
     unzip mongodb.zip
     cd mongodb-main
-    mongo --ssl --host ${aws_docdb_cluster.default.endpoint} --sslCAFile rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < catalogue.js
+    mongo --ssl --host ${aws_docdb_cluster.default.endpoint} --sslCAFile rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < catalogue.js"
     mongo --ssl --host ${aws_docdb_cluster.default.endpoint} --sslCAFile rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < users.js
     EOF
   }
