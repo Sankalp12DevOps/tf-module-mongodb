bf6c1b39dda287fdd340c88a8e73322b28842283
Sankalp12DevOps <sravanthi388@gmail.com>
2023-04-28 13:07:37 +0530

    m

--- provisioner.tf
+++ provisioner.tf
@@ -1,15 +1,15 @@
-resource "null_resource" "web" {
-  depends_on = [aws_db_instance.mysql]
-  provisioner "local-exec" {
-    command = <<EOF
-    curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
-    cd /tmp
-    unzip mongodb.zip
-    cd mongodb-main
-    mongo < catalogue.js
-    mongo < users.js
-    EOF
-  }
-}
+# resource "null_resource" "web" {
+#   depends_on = [aws_db_instance.mysql]
+#   provisioner "local-exec" {
+#     command = <<EOF
+#     curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+#     cd /tmp
+#     unzip mongodb.zip
+#     cd mongodb-main
+#     mongo < catalogue.js
+#     mongo < users.js
+#     EOF
+#   }
+# }
 
 
