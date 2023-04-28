420cc76f66bb1ea51c155a592e86348f0b97f038
Sankalp12DevOps <sravanthi388@gmail.com>
2023-04-28 15:14:53 +0530

    m

--- provisioner.tf
+++ provisioner.tf
@@ -1,11 +1,12 @@
 resource "null_resource" "web" {
+    depends_on = [aws_docdb_cluster.default]
   provisioner "local-exec" {
     command = <<EOF
     curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
     cd /tmp
     unzip mongodb.zip
     cd mongodb-main
-    mongo --ssl --host ${aws_docdb_cluster.default.endpoint} --sslCAFile rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < catalogue.js"
+    mongo --ssl --host ${aws_docdb_cluster.default.endpoint} --sslCAFile rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < catalogue.js
     mongo --ssl --host ${aws_docdb_cluster.default.endpoint} --sslCAFile rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < users.js
     EOF
   }
