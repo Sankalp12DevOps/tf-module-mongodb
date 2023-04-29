fe92d7790c765249ce6f9eb347aba40f992af4bf
Sankalp12DevOps <sravanthi388@gmail.com>
2023-04-29 12:23:40 +0530

    m

--- route.tf
+++ route.tf
@@ -2,6 +2,6 @@ resource "aws_route53_record" "mysqlroute" {
   zone_id = data.terraform_remote_state.vpc.outputs.PUBLIC_HOSTED_ZONE_ID
   name    = "docdb-${var.ENV}"
   type    = "CNAME"
-  records = [aws_db_instance.mysql.endpoint]
+  records = [aws_docdb_cluster.default.endpoint]
   ttl     = 5
 }
